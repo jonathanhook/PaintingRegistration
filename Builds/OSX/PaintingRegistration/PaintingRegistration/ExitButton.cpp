4f769ba4dfbbc39b76687a596a59c6e86f61a163
jonathanhook <jonathan.hook@ncl.ac.uk>
2012-11-22 17:31:02 +0000

    Some deletes

--- Builds/OSX/PaintingRegistration/PaintingRegistration/ExitButton.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  ExitButton.cpp
-//  PaintingRegistration
-//
-//  Created by Jonathan Hook on 20/11/2012.
-//  Copyright (c) 2012 Jonathan Hook. All rights reserved.
-//
-
-#include "ExitButton.h"
